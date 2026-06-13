f854652f067935d767ed92adf56ac910829c3fb4
gabrielpirovanemiguel <gabriel.balique@gmail.com>
2026-06-13 13:48:42 -0300

    fix: adiciona validação antes da conversão e melhora sua legibilidade

--- ex1.js
+++ ex1.js
@@ -1,6 +1,10 @@
 import printConsoleScreen from "./printConsoleScreen.js";
-let reais = parseFloat(prompt('Digite um valor em reais: '));
+let reais;
+do {
+    reais = parseFloat(prompt('Digite um valor em reais: '));
+} while (isNaN(reais) || reais <= 0);
 const euroRate = 6.1;
 const dolarRate = 5.7;
-printConsoleScreen(`R$${reais} em euros: €${(reais / euroRate).toFixed(2)}`);
-printConsoleScreen(`R$${reais} em dólares: $${(reais / dolarRate).toFixed(2)}`);
+const euros = (reais / euroRate).toFixed(2);
+const dolars = (reais / dolarRate).toFixed(2);
+printConsoleScreen(`Converção de R$${reais}:\nPara euros: €${euros}\nPara dólares: $${dolars}`);
